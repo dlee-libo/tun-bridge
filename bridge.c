5e3e9a56db6cf9a60b4f3934472c1a7996f24b8a
Bo Li <libo@seagroup.com>
2020-05-23 14:08:23 +0800

    remove unused include

--- bridge.c
+++ bridge.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/time.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
